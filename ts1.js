8c27ce5c6b80a4de0eebf4452f9c56b851f8ec33
Timchen7 <timchen7@yahoo.com>
2017-03-14 13:38:34 +0000

    type

--- ts1.js
+++ ts1.js
@@ -2,4 +2,36 @@ function hi() {
     console.log("Hi ts");
 }
 hi();
+var fullName = "T C";
+var paidAccount = true;
+var versionNumber = 1.3;
+fullName = "Next C";
+paidAccount = false;
+console.log(fullName);
+console.log(paidAccount);
+console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi ";
+    console.log(greeting + f + " " + l);
+}
+printName("Nordan", "Chen");
+var p1 = ["aa", "bb", "cc"];
+var p2 = [1, 2, 3];
+console.log(p1);
+console.log(p2);
+var names;
+names = ["Jordan Hudgens", "Tiffany Hudgens"];
+console.log(names);
+names = "Kristine Hudgens";
+console.log(names);
+var password = 'zxcvzxcv';
+if (password == 'asdfasdf') {
+    console.log('Yes, asdfasdf is the password');
+}
+else if (password == 'zxcvzxcv') {
+    console.log('Yes, zxcvzxcv is the password');
+}
+else {
+    console.log('Sorry, permission denied');
+}
 //# sourceMappingURL=ts1.js.map
--- ts1.ts
+++ ts1.ts
@@ -3,3 +3,42 @@ function hi() {
 }
 
 hi();
+
+var fullName : string = "T C";
+let paidAccount : boolean = true;
+const versionNumber : number = 1.3;
+fullName = "Next C";
+paidAccount = false;
+console.log(fullName);
+console.log(paidAccount);
+console.log(versionNumber);
+
+function printName(f,l) {
+  let greeting : string = "Hi ";
+  console.log(greeting + f + " " + l);
+}
+printName("Nordan","Chen");
+
+type Ar1 = Array<string|number>;
+let p1 : Ar1 = ["aa","bb","cc"];
+let p2 : Ar1 = [1,2,3];
+console.log(p1);
+console.log(p2);
+
+var names : string[]|string;
+names = ["Jordan Hudgens", "Tiffany Hudgens"];
+console.log(names);
+names = "Kristine Hudgens";
+console.log(names);
+
+let password : string = 'zxcvzxcv';
+if(password == 'asdfasdf') {
+	console.log('Yes, asdfasdf is the password');
+} else if(password == 'zxcvzxcv') {
+	console.log('Yes, zxcvzxcv is the password');
+} else {
+	console.log('Sorry, permission denied');
+}
+
+
+
